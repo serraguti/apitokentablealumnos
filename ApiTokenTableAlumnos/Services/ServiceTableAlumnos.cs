134fccace73b6e8b18c76f38698a53ba93d2654f
serraguti <pacoserranox@gmail.com>
2021-03-11 10:24:33 +0100

    commit

--- ApiTokenTableAlumnos/Services/ServiceTableAlumnos.cs
+++ ApiTokenTableAlumnos/Services/ServiceTableAlumnos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,16 @@ namespace ApiTokenTableAlumnos.Services
 {
     public class ServiceTableAlumnos
     {
+        private CloudTable tablaalumnos;
+
+        public ServiceTableAlumnos(String keys)
+        {
+            CloudStorageAccount account =
+                CloudStorageAccount.Parse(keys);
+            CloudTableClient client =
+                account.CreateCloudTableClient();
+            this.tablaalumnos =
+                client.GetTableReference("tablaalumnos");
+        }
     }
 }
